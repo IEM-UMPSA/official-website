d3c30206aa0bd3e0a8e7fe70f05f4600341e05c6
Hovah <hovahyii@gmail.com>
2022-12-30 13:56:02 +0800

    rename jsx to js
    Signed-off-by: Hovah <hovahyii@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,8 @@ import ErrorBoundary from '../components/ErrorBoundary';
 
 class MyApp extends App {
 
-    static async getStaticProps ({ Component, ctx }) {
-        const pageProps = Component.getStaticProps  ? await Component.getStaticProps (ctx) : {};
+    static async getInitialProps({ Component, ctx }) {
+        const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
         //Anything returned here can be accessed by the client
         return { pageProps: pageProps };
