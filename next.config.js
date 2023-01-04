28a5facee53879182ab39d0592b3ae6755bbba1b
Hovah <hovahyii@gmail.com>
2023-01-04 20:57:40 +0800

    edit next.config.js
    Signed-off-by: Hovah <hovahyii@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,20 +4,9 @@ module.exports = {
   publicRuntimeConfig:{
     APP_NAME: 'IEM-UMPSS',
     API_DEVELOPMENT: 'http://localhost:3000/api',
-    API_PRODUCTION: 'http://iemumpss.vercel.app/',
+    API_PRODUCTION: 'https://iemumpss.vercel.app/',
     PRODUCTION: true,
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-    DOMAIN_PRODUCTION: 'http://iemumpss.vercel.app/',
-  },
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
+    DOMAIN_PRODUCTION: 'https://iemumpss.vercel.app/',
   }
 };
