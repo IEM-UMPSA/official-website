425f17595c91309328d0c1240276704c363f1026
Hovah <hovahyii@gmail.com>
2023-02-07 23:21:34 +0800

    added google analytics
    Signed-off-by: Hovah <hovahyii@gmail.com>

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,15 @@ export default function Document() {
       <Head>
             <link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
             <link rel="icon" href="./assets/img/favicon.ico" />
+            {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BRBBYXSR48"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BRBBYXSR48');
+          </script>
       </Head>
       <body>
         <Main />
