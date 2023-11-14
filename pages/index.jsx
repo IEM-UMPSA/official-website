e827bdb59e94404547de1ffb4331579c337fee22
Jehovah Yii Zui Hon (Hovah) <hovahyii@gmail.com>
2023-11-15 00:48:12 +0800

    change url (#11)
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ class Index extends React.Component {
                 pageDescription="Join IEM-UMPSS, the Institution of Engineers Malaysia – University of Malaysia Pahang Student Section, and connect with engineers, institutions, and professionals. Celebrating 10 years, IEM-UMPSS provides students opportunities for professional development and exposure to the engineering industry." 
                 pageKeywords="iemumpss, iem-umpss, iem ump, IEM-UMPSS, Institution of Engineers Malaysia, University of Malaysia Pahang Student Section, engineering, students, professional development, exposure, engineers, institutions, professionals, workshops, site visits, conferences, 10 years anniversary"
                 pageImage="/public/assets/img/main.jpg"
-                pageURL="https://iemumpss.vercel.app"
+                pageURL="https://iemumpss.tech"
                 pageImageAlt="IEM-UMPSS Main Page"
             />
             <Header />
