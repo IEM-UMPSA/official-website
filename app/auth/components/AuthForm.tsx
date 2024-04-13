a89a1b35ac5f450556fb1de52f32b507548f5793
hovahyii <hovahyii@gmail.com>
2024-04-13 15:40:37 +0800

    update zodResolver
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- app/auth/components/AuthForm.tsx
+++ app/auth/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod/dist/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
